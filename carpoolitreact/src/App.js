f9a9b213ee47e7409d9f0cdad6323f970a87c641
Elenaaa7 <152070757+Elenaaa7@users.noreply.github.com>
2023-12-22 21:17:46 +0200

    Revert "Staging"

--- carpoolitreact/src/App.js
+++ carpoolitreact/src/App.js
@@ -1,21 +1,23 @@
-// App.js
-import React, { useState } from "react";
-import "./App.css";
-import Login from "./pages/Login";
-import Main from "./pages/Main";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // Function to handle successful login
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
-
   return (
     <div className="App">
-      {!isAuthenticated && <Login onLogin={handleLogin} />}
-      {isAuthenticated && <Main />}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
